daa73b4ae8807256309d0240193856ab20502a59
Bluebaloo <29752757+Bluebaloo@users.noreply.github.com>
2020-04-19 17:29:32 +0900

    abcp

--- ABC/abc079/c/main.cpp
+++ ABC/abc079/c/main.cpp
@@ -1,17 +1,40 @@
 #include <bits/stdc++.h>
 
-
 using namespace std;
 
 void solve() {
+  int a, b, c, d;
+  cin >> a;
+  d = a % 10;
+  a /= 10;
+  c = a % 10;
+  a /= 10;
+  b = a % 10;
+  a /= 10;
 
+  if (a + b + c + d == 7)
+    cout << a << '+' << b << '+' << c << '+' << d << "=7" << endl;
+  else if (a + b + c - d == 7)
+    cout << a << '+' << b << '+' << c << '-' << d << "=7" << endl;
+  else if (a + b - c + d == 7)
+    cout << a << '+' << b << '-' << c << '+' << d << "=7" << endl;
+  else if (a + b - c - d == 7)
+    cout << a << '+' << b << '-' << c << '-' << d << "=7" << endl;
+  else if (a - b + c + d == 7)
+    cout << a << '-' << b << '+' << c << '+' << d << "=7" << endl;
+  else if (a - b + c - d == 7)
+    cout << a << '-' << b << '+' << c << '-' << d << "=7" << endl;
+  else if (a - b - c + d == 7)
+    cout << a << '-' << b << '-' << c << '+' << d << "=7" << endl;
+  else if (a - b - c - d == 7)
+    cout << a << '-' << b << '-' << c << '-' << d << "=7" << endl;
 }
 
 int main() {
-    cin.tie(nullptr);
-    ios::sync_with_stdio(false);
-    std::cout << std::fixed << std::setprecision(15);
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(15);
 
-    solve();
-    return 0;
+  solve();
+  return 0;
 }
