a5c8c6c68eb266ccc655439fa09c28a763b9b6d3
Bluebaloo <29752757+Bluebaloo@users.noreply.github.com>
2020-04-24 23:04:22 +0900

    abcp

--- ABC/abc139/c/main.cpp
+++ ABC/abc139/c/main.cpp
@@ -1,19 +1,50 @@
 #include <bits/stdc++.h>
 
-#define whole(f, x, ...) ([&](decltype((x)) whole) { return (f)(begin(whole), end(whole), ## __VA_ARGS__); })(x)
-
+#define whole(f, x, ...)                                 \
+  ([&](decltype((x)) whole) {                            \
+    return (f)(begin(whole), end(whole), ##__VA_ARGS__); \
+  })(x)
 
 using namespace std;
 using ll = long long;
 
 void solve() {
+  int n;
+  cin >> n;
+
+  if (n == 1) {
+    cout << "0\n";
+    return;
+  }
+
+  int ma = 0;
+  int cnt = 0;
+  ll h, oh;
+  cin >> oh >> h;
+  if (h <= oh) {
+    cnt = 1;
+    ma = 1;
+  }
+  oh = h;
+
+  for (int i = 2; i < n; i++) {
+    cin >> h;
+    if (h <= oh) {
+      ++cnt;
+    } else {
+      ma = max(ma, cnt);
+      cnt = 0;
+    }
+    oh = h;
+  }
+  cout << max(ma, cnt) << endl;
 }
 
 int main() {
-    cin.tie(nullptr);
-    ios::sync_with_stdio(false);
-    std::cout << std::fixed << std::setprecision(15);
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(15);
 
-    solve();
-    return 0;
+  solve();
+  return 0;
 }
