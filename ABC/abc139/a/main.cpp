05cb62c3675dfc14271634d6e995cb4c0e2da0e7
Bluebaloo <29752757+Bluebaloo@users.noreply.github.com>
2020-04-25 23:22:07 +0900

    abcp

--- ABC/abc139/a/main.cpp
+++ ABC/abc139/a/main.cpp
@@ -1,19 +1,30 @@
 #include <bits/stdc++.h>
 
-#define whole(f, x, ...) ([&](decltype((x)) whole) { return (f)(begin(whole), end(whole), ## __VA_ARGS__); })(x)
-
+#define whole(f, x, ...)                                 \
+  ([&](decltype((x)) whole) {                            \
+    return (f)(begin(whole), end(whole), ##__VA_ARGS__); \
+  })(x)
 
 using namespace std;
 using ll = long long;
 
 void solve() {
+  string s1, s2;
+  cin >> s1 >> s2;
+  int cnt = 0;
+  for (int i = 0; i < 3; i++) {
+    if (s1[i] == s2[i]) {
+      ++cnt;
+    }
+  }
+  cout << cnt << endl;
 }
 
 int main() {
-    cin.tie(nullptr);
-    ios::sync_with_stdio(false);
-    std::cout << std::fixed << std::setprecision(15);
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(15);
 
-    solve();
-    return 0;
+  solve();
+  return 0;
 }
--- ABC/abc139/d/main.cpp
+++ ABC/abc139/d/main.cpp
@@ -1,19 +1,24 @@
 #include <bits/stdc++.h>
 
-#define whole(f, x, ...) ([&](decltype((x)) whole) { return (f)(begin(whole), end(whole), ## __VA_ARGS__); })(x)
-
+#define whole(f, x, ...)                                 \
+  ([&](decltype((x)) whole) {                            \
+    return (f)(begin(whole), end(whole), ##__VA_ARGS__); \
+  })(x)
 
 using namespace std;
 using ll = long long;
 
 void solve() {
+  ll n;
+  cin >> n;
+  cout << n * (n - 1) / 2 << endl;
 }
 
 int main() {
-    cin.tie(nullptr);
-    ios::sync_with_stdio(false);
-    std::cout << std::fixed << std::setprecision(15);
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(15);
 
-    solve();
-    return 0;
+  solve();
+  return 0;
 }
