eb78688f689806db9b456ca7579a384140326899
Bluebaloo <29752757+Bluebaloo@users.noreply.github.com>
2020-04-27 18:48:23 +0900

    Math

--- ABC/abc164/d/main.cpp
+++ ABC/abc164/d/main.cpp
@@ -11,18 +11,30 @@ using ll = long long;
 void solve() {
   string s;
   cin >> s;
+  reverse(s.begin(), s.end());
+  vector<int> v(2019);
 
-  int cnt = 0;
-  for (int i = 1; i < 1e4; i++) {
-    string ser = to_string(i * 2019);
-    size_t pos = s.find(ser);
-    while (pos != string::npos) {
-      pos = s.find(ser, pos + 1);
-      ++cnt;
-    }
+  // int t = s[0] - '0';
+  // int ten = 10;
+  // ++v[t];
+
+  int t = 0, ten = 1;
+  ++v[t];
+
+  for (auto &c : s) {
+    t += ten * (c - '0');
+    t %= 2019;
+    ++v[t];
+    ten *= 10;
+    ten %= 2019;
   }
 
-  cout << cnt << endl;
+  ll ans = 0;
+  for (int i : v) {
+    ans += i * (i - 1) / 2;
+  }
+
+  cout << ans << endl;
 }
 
 int main() {
