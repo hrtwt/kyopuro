117eedd3b8150063a5c91f134668526d76cb6df0
Bluebaloo <29752757+Bluebaloo@users.noreply.github.com>
2020-04-30 16:06:02 +0900

    abcp

---
+++ ABC/abc156/b/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+#define whole(f, x, ...)                                 \
+  ([&](decltype((x)) whole) {                            \
+    return (f)(begin(whole), end(whole), ##__VA_ARGS__); \
+  })(x)
+
+using namespace std;
+using ll = long long;
+
+void solve() {
+  ll n;
+  int k;
+  cin >> n >> k;
+  ll cnt = 0;
+  while (n != 0) {
+    n /= k;
+    ++cnt;
+  }
+  cout << cnt << endl;
+}
+
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(15);
+
+  solve();
+  return 0;
+}
