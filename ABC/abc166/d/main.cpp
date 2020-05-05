ba7bc406d16ae71f940f6ba8eb19655e4b8863f5
Bluebaloo <29752757+Bluebaloo@users.noreply.github.com>
2020-05-05 16:08:48 +0900

    abcp

--- ABC/abc166/d/main.cpp
+++ ABC/abc166/d/main.cpp
@@ -1,19 +1,35 @@
 #include <bits/stdc++.h>
 
-#define whole(f, x, ...) ([&](decltype((x)) whole) { return (f)(begin(whole), end(whole), ## __VA_ARGS__); })(x)
-
+#define whole(f, x, ...)                                 \
+  ([&](decltype((x)) whole) {                            \
+    return (f)(begin(whole), end(whole), ##__VA_ARGS__); \
+  })(x)
 
 using namespace std;
 using ll = long long;
 
 void solve() {
+  ll x;
+  cin >> x;
+  const int lim = 1e3;
+
+  auto pow5 = [](auto t) { return t * t * t * t * t; };
+
+  for (int i = 0; i < lim; i++) {
+    for (int j = -lim; j < lim; j++) {
+      if (pow5(i) - pow5(j) == x) {
+        cout << i << ' ' << j << endl;
+        return;
+      }
+    }
+  }
 }
 
 int main() {
-    cin.tie(nullptr);
-    ios::sync_with_stdio(false);
-    std::cout << std::fixed << std::setprecision(15);
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(15);
 
-    solve();
-    return 0;
+  solve();
+  return 0;
 }
--- ABC/abc166/e/main.cpp
+++ ABC/abc166/e/main.cpp
@@ -11,25 +11,18 @@ using ll = long long;
 void solve() {
   int n;
   cin >> n;
-  multimap<ll, int> a;
+  unordered_map<ll, int> ad, su;
 
-  for (int i = 0; i < n; i++) {
-    ll in;
-    cin >> in;
-    a.insert({in, i});
+  ll cnt = 0;
+
+  for (int i = 1; i <= n; i++) {
+    ll h;
+    cin >> h;
+
+    cnt += ad[i - h];
+    ++ad[h + i];
   }
 
-  int cnt = 0;
-  for (auto it = a.begin(); it != a.end(); it = next(it)) {
-    for (auto itt = it; itt != a.end(); itt = next(itt)) {
-      if (abs(itt->second - it->second) == itt->first + it->first) {
-        ++cnt;
-      }
-      if (itt->first + it->first > n) {
-        break;
-      }
-    }
-  }
   cout << cnt << endl;
 }
 
