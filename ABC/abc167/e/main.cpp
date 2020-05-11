684a5cf1a14a82fe3ca07245a9677afa16d08a8d
Bluebaloo <29752757+Bluebaloo@users.noreply.github.com>
2020-05-11 23:04:59 +0900

    abcp

--- ABC/abc167/e/main.cpp
+++ ABC/abc167/e/main.cpp
@@ -10,26 +10,127 @@ using ll = long long;
 
 const int mod = 998244353;
 
+template <int mod>
+struct ModInt {
+  int x;
+
+  ModInt() : x(0) {}
+
+  ModInt(int64_t y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+
+  ModInt &operator+=(const ModInt &p) {
+    if ((x += p.x) >= mod) x -= mod;
+    return *this;
+  }
+
+  ModInt &operator-=(const ModInt &p) {
+    if ((x += mod - p.x) >= mod) x -= mod;
+    return *this;
+  }
+
+  ModInt &operator*=(const ModInt &p) {
+    x = (int)(1LL * x * p.x % mod);
+    return *this;
+  }
+
+  ModInt &operator/=(const ModInt &p) {
+    *this *= p.inverse();
+    return *this;
+  }
+
+  ModInt operator-() const { return ModInt(-x); }
+
+  ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
+
+  ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
+
+  ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
+
+  ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
+
+  bool operator==(const ModInt &p) const { return x == p.x; }
+
+  bool operator!=(const ModInt &p) const { return x != p.x; }
+
+  ModInt inverse() const {
+    int a = x, b = mod, u = 1, v = 0, t;
+    while (b > 0) {
+      t = a / b;
+      swap(a -= t * b, b);
+      swap(u -= t * v, v);
+    }
+    return ModInt(u);
+  }
+
+  ModInt pow(int64_t n) const {
+    ModInt ret(1), mul(x);
+    while (n > 0) {
+      if (n & 1) ret *= mul;
+      mul *= mul;
+      n >>= 1;
+    }
+    return ret;
+  }
+
+  friend ostream &operator<<(ostream &os, const ModInt &p) { return os << p.x; }
+
+  friend istream &operator>>(istream &is, ModInt &a) {
+    int64_t t;
+    is >> t;
+    a = ModInt<mod>(t);
+    return (is);
+  }
+
+  static int get_mod() { return mod; }
+};
+
+using modint = ModInt<mod>;
+
+template <typename T>
+struct Combination {
+  vector<T> _fact, _rfact, _inv;
+
+  Combination(int sz) : _fact(sz + 1), _rfact(sz + 1), _inv(sz + 1) {
+    _fact[0] = _rfact[sz] = _inv[0] = 1;
+    for (int i = 1; i <= sz; i++) _fact[i] = _fact[i - 1] * i;
+    _rfact[sz] /= _fact[sz];
+    for (int i = sz - 1; i >= 0; i--) _rfact[i] = _rfact[i + 1] * (i + 1);
+    for (int i = 1; i <= sz; i++) _inv[i] = _rfact[i] * _fact[i - 1];
+  }
+
+  inline T fact(int k) const { return _fact[k]; }
+
+  inline T rfact(int k) const { return _rfact[k]; }
+
+  inline T inv(int k) const { return _inv[k]; }
+
+  T P(int n, int r) const {
+    if (r < 0 || n < r) return 0;
+    return fact(n) * rfact(n - r);
+  }
+
+  T C(int p, int q) const {
+    if (q < 0 || p < q) return 0;
+    return fact(p) * rfact(q) * rfact(p - q);
+  }
+
+  T H(int n, int r) const {
+    if (n < 0 || r < 0) return (0);
+    return r == 0 ? 1 : C(n + r - 1, r);
+  }
+};
+
 void solve() {
-  int n, m, k;
+  modint n, m, k;
   cin >> n >> m >> k;
 
-  vector<vector<int>> dp(n + 1, vector<int>(k + 1));
+  modint ans(0);
+  Combination<modint> c(n.x);
 
-  dp[1][0] = m;
-
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j <= k; j++) {
-      dp[i + 1][j] = max(dp[i + 1][j], ((m - 1) * dp[i][j]) % mod);
-      dp[i + 1][j + 1] = max(dp[i + 1][j + 1], (m * dp[i][j]) % mod);
-    }
+  for (int i = 0; i <= k.x; i++) {
+    ans += m * (m - 1).pow(n.x - i - 1) * c.C(n.x - 1, i);
   }
 
-  int ans = 0;
-  for (int i = 0; i < k + 1; i++) {
-    ans += dp[n][i] % mod;
-    ans %= mod;
-  }
   cout << ans << endl;
 }
 
