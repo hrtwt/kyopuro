3789cd36a753d1e652a2287dccfc01e53d429d08
hrtwt <29752757+hrtwt@users.noreply.github.com>
2020-06-05 23:30:17 +0900

    数学

---
+++ ABC/abc145/d/main.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+const int MOD = 1e9 + 7;
+
+template <int mod>
+struct ModInt {
+  int x;
+
+  ModInt() : x(0) {}
+
+  ModInt(int64_t y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+
+  ModInt &operator+=(const ModInt &p) {
+    if ((x += p.x) >= mod) x -= mod;
+    return *this;
+  }
+
+  ModInt &operator-=(const ModInt &p) {
+    if ((x += mod - p.x) >= mod) x -= mod;
+    return *this;
+  }
+
+  ModInt &operator*=(const ModInt &p) {
+    x = (int)(1LL * x * p.x % mod);
+    return *this;
+  }
+
+  ModInt &operator/=(const ModInt &p) {
+    *this *= p.inverse();
+    return *this;
+  }
+
+  ModInt operator-() const { return ModInt(-x); }
+
+  ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
+
+  ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
+
+  ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
+
+  ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
+
+  bool operator==(const ModInt &p) const { return x == p.x; }
+
+  bool operator!=(const ModInt &p) const { return x != p.x; }
+
+  ModInt inverse() const {
+    int a = x, b = mod, u = 1, v = 0, t;
+    while (b > 0) {
+      t = a / b;
+      swap(a -= t * b, b);
+      swap(u -= t * v, v);
+    }
+    return ModInt(u);
+  }
+
+  ModInt pow(int64_t n) const {
+    ModInt ret(1), mul(x);
+    while (n > 0) {
+      if (n & 1) ret *= mul;
+      mul *= mul;
+      n >>= 1;
+    }
+    return ret;
+  }
+
+  friend ostream &operator<<(ostream &os, const ModInt &p) { return os << p.x; }
+
+  friend istream &operator>>(istream &is, ModInt &a) {
+    int64_t t;
+    is >> t;
+    a = ModInt<mod>(t);
+    return (is);
+  }
+
+  static int get_mod() { return mod; }
+};
+
+using modint = ModInt<MOD>;
+
+void solve() {
+  int x, y;
+  cin >> x >> y;
+  if ((2 * x - y) % 3 != 0 || (2 * y - x) % 3 != 0) {
+    cout << 0 << endl;
+    return;
+  }
+  const int s = max((2 * x - y) / 3, (2 * y - x) / 3);
+  const int t = min((2 * x - y) / 3, (2 * y - x) / 3);
+
+  if (s < 0 || t < 0) {
+    cout << 0 << endl;
+    return;
+  }
+
+  modint ans(1);
+  for (int i = s + 1; i <= s + t; i++) {
+    ans *= i;
+  }
+  for (int i = 2; i <= t; i++) {
+    ans /= i;
+  }
+  cout << ans << endl;
+}
+
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(15);
+
+  solve();
+  return 0;
+}
