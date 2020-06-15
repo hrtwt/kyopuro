d8b14f537b065a04d54ed2522e06189605c2b45a
hrtwt <29752757+hrtwt@users.noreply.github.com>
2020-06-15 15:14:17 +0900

    エラトステネスの篩

--- ABC/abc170/d/main.cpp
+++ ABC/abc170/d/main.cpp
@@ -4,50 +4,32 @@ using namespace std;
 using ll = long long;
 using ld = long double;
 
-bool is_prime(int x) {
-  for (int i = 2; i * i <= x; i++) {
-    if (x % i == 0) return false;
-  }
-  return true;
-}
-
 void solve() {
   int n;
   cin >> n;
-
-  if (n == 1) {
-    cout << 1 << endl;
-  }
-
-  vector<int> a(n);
+  unordered_map<int, int> b;
+  unordered_set<int> c;
+  vector<bool> d(1e6 + 1, true);
   for (int i = 0; i < n; i++) {
-    cin >> a[i];
+    int t;
+    cin >> t;
+    ++b[t];
+    c.insert(t);
   }
 
-  sort(a.begin(), a.end());
-
-  int ans = a[0] != a[1] ? 1 : 0;
-
-  for (int i = 1; i < n; i++) {
-    // if (a[i - 1] == a[i] || (i != n - 1 && a[i] == a[i + 1])) {
-    //   continue;
-    // }
-    if (is_prime(a[i])) {
-      ++ans;
-      continue;
-    }
-
-    bool ok = true;
-    for (int j = 0; j < i; j++) {
-      if (a[i] % a[j] == 0) {
-        ok = false;
-        break;
-      }
-    }
-    if (ok) {
-      ++ans;
+  for (auto &i : c) {
+    int t = 2;
+    while (i * t <= 1e6) {
+      d[i * t] = false;
+      ++t;
     }
   }
+
+  int ans = 0;
+  for (auto &i : c) {
+    if (b[i] == 1 && d[i]) ++ans;
+  }
+
   cout << ans << endl;
 }
 
