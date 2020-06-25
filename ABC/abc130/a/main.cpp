1a204cad2cfaadc426aa7ef1ab4d14297478f852
hrtwt <29752757+hrtwt@users.noreply.github.com>
2020-06-25 23:43:48 +0900

    やるだけ

---
+++ ABC/abc130/a/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+void solve() {
+  int x, a;
+  cin >> x >> a;
+
+  if (x < a) {
+    cout << 0 << endl;
+  } else {
+    cout << 10 << endl;
+  }
+}
+
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(15);
+
+  solve();
+  return 0;
+}
